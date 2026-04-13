fb5b24eed7c8a5507f8059877e0cbe271d9a5371
LorenzoColombi <lorenzo.colombi@unife.it>
2026-04-13 11:07:19 +0200

    first commit

---
+++ quickstart-pytorch/pytorchexample/client_app.py
@@ -0,0 +1,25 @@
+"""pytorchexample: A Flower / PyTorch app."""
+
+import torch
+from flwr.app import ArrayRecord, Context, Message, MetricRecord, RecordDict
+from flwr.clientapp import ClientApp
+
+# Import task functions defined in task.py
+from pytorchexample.task import Net, load_data
+from pytorchexample.task import test as test_fn
+from pytorchexample.task import train as train_fn
+
+# Flower ClientApp
+app = ClientApp()
+
+
+@app.train()
+def train(msg: Message, context: Context):
+    """Train the model on local data."""
+    TODO
+
+
+@app.evaluate()
+def evaluate(msg: Message, context: Context):
+    """Evaluate the model on local data."""
+    TODO
